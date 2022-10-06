c945eba06d8901850f40ed994a5f507665b1fdc9
NARANJO GUTIERREZ GUTIERREZ <113268603+TheLeck@users.noreply.github.com>
2022-10-06 04:17:52 +0000

    Los dos ejercicios

---
+++ 2p/3stack/2do/2do.js
@@ -0,0 +1,76 @@
+class Stack {
+    constructor(){
+        this.data = []
+        this.top = 5//stack size
+    }
+
+    push(o){//validate stack not full
+        if (this.isFull())
+            console.log('full stack')
+        else{
+            this.data.push(o)
+            console.log(o,'added')
+        }
+    }
+
+    peek(){
+        return this.data[this.data.length-1]
+    }
+
+    pop(){//validate stack not empty
+        if (this.isEmpty())
+            console.log('empty stack')
+        else
+            console.log(this.data.pop(),'popped')
+    }
+
+    isEmpty(){
+        if (this.data.length === 0)//this.size()
+            return true
+        else
+            return false
+    }
+
+    isFull(){
+        if (this.data.length === this.top)
+            return true
+        else
+            return false
+    }
+
+    size(){
+     return this.data.length
+    }
+
+    search(k){
+      let cont = 0, ind = 0
+      for (let i = 0;i<this.data.length;i++){
+        if(this.data[i] == k){
+          cont++
+          ind = i
+        }
+      }
+      if (cont != 0){
+        console.log('Encontrado en la posicion: ', ind)
+      }else{
+          console.log('No encontrado')
+      }
+    }
+    traverse(){}
+
+    toString(){}
+    makeEmpty(){
+        this.data = []
+    }
+}
+
+let s = new Stack()
+s.push(4)
+//s.search(4)
+s.push(0)
+s.push(3)
+s.push(2)
+s.push(1)
+s.push(5)
+console.log('size',s.size())
+s.search(1)
--- 2p/4queue/2do/2.4queue-solution.js
+++ 2p/4queue/2do/2.4queue-solution.js
@@ -3,3 +3,86 @@
  * your solution here
  *
  */
+
+function Queue() {
+    this.dataStore = []
+  this.top = 5
+
+    this.enqueue= function(element) {
+      if (this.dataStore.length == this.top){
+        console.log("Fila llena")
+      } else {
+        this.dataStore.push(element)
+      }
+    }
+
+    this.dequeue= function() {
+      if (this.dataStore.length == 0){
+        console.log("Fila vacia")
+      } else {
+        return this.dataStore.shift()
+      }
+	
+    }
+
+    this.front= function() {
+	return this.dataStore[0]
+    }
+
+    this.back= function() {
+	return this.dataStore[this.dataStore.length-1]
+    }
+
+    this.toString= function() {
+	let retStr = ""
+	for (let i = 0; i < this.dataStore.length; ++i) {
+	    retStr += this.dataStore[i] + "\n"
+	}
+	return retStr
+    }
+
+    this.empty= function() {
+	if (this.dataStore.length == 0){
+	    console.log("velda")
+    return true	
+	}else{ 
+      console.log("no velda")
+	    return false
+	}
+    }
+
+    this.full= function() {
+      if (this.dataStore.length == this.top){
+        console.log("velda")
+        return true
+      } else {
+        console.log("no velda")
+        return false
+      }
+    }
+}
+
+
+let q = new Queue()
+q.enqueue("Meredith")
+q.enqueue("Cynthia")
+q.enqueue("Jennifer")
+q.enqueue("Pepito")
+q.enqueue("Gloria")
+q.enqueue("Justin")
+q.empty()
+q.full()
+console.log(q.toString())
+
+console.log("Front of queue: " + q.front())
+console.log("Back of queue: " + q.back())
+
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+console.log(q.toString())
+
+console.log('done')
